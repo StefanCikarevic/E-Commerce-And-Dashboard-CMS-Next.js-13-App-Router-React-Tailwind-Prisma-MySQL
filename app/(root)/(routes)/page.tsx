e1b0acaa73430b61e8851e651151c92532658b14
StefanCikarevic <stefan.cikarevic@prodyna.com>
2024-01-11 14:46:27 +0100

    first commit

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+export default function Home() {
+  const { onOpen, isOpen } = useStoreModal();
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
+}
